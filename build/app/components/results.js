c3eb437ffd8efe3143a0f5f99332bb16be5a387d
Albert Xu <thealbertxu@gmail.com>
2016-11-13 16:46:52 -0500

    results

--- build/app/components/results.js
+++ build/app/components/results.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export default class results extends React.Component {
   constructor(props) {
@@ -18,12 +19,14 @@ export default class results extends React.Component {
               <div className="panel-body">
                 <div className="media">
                   <div className="media-left">
-                    <a href="#">
+                    <Link to={"/recipes/" + data.name}>
                       <img className="media-object result-pic" src="img/brownie-egg.jpg" alt="brownie-egg"></img>
-                    </a>
+                    </Link>
                   </div>
                   <div className="media-body">
-                    <h4 className="media-heading">{data.name}</h4>
+                    <h4 className="media-heading">
+                      <Link to={"/recipes/" + data.name}>{data.name}</Link>
+                    </h4>
                     {data.description}
                   </div>
                   <div className="media-right recipe-vote">
