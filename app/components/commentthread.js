8a302a793dfdbc04115f2a6038aa0819502a9e57
Chang Ding Yang <oscaryang12@yahoo.com>
2016-11-14 15:08:16 -0500

    statusupdate and commentthread

---
+++ app/components/commentthread.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CommentEntry from './commententry';
+
+export default class CommentThread extends React.Component {
+  render() {
+    return (
+    <ul className="media-list">
+      {React.Children.map(this.props.children, function(child) {
+        return (
+        <li className="media">
+
+          {child}
+        </li>
+        )
+      })}
+      <li className="media">
+      <CommentEntry onPost={this.props.onPost} />
+      </li>
+    </ul>
+    )
+  }
+}
---
+++ app/components/statusupdate.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {unixTimeToString} from '../util.js';
+import {Link} from 'react-router';
+
+export default class StatusUpdate extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-10">
+            <div className="media">
+              <div className="media-left media-top">
+              PIC
+              </div>
+              <div className="media-body">
+                <Link to={"/profile/" + this.props.author._id}>
+                {this.props.author.fullName}
+                </Link>
+              <br /> {unixTimeToString(this.props.postDate)} · {this.props.location} · <span
+              className="glyphicon glyphicon-user"></span>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-2">
+          <span className="caret pull-right"></span>
+          </div>
+        </div>
+        
+        <div className="row">
+          <div className="col-md-12">
+          {this.props.children}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
