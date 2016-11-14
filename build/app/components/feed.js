c1bfd970c174a8991ecb42bce04debc0812e72ee
Chang Ding Yang <oscaryang12@yahoo.com>
2016-11-14 00:57:18 -0500

    Fix for feed

--- build/app/components/feed.js
+++ build/app/components/feed.js
@@ -23,66 +23,66 @@ export default class Feed extends React.Component {
         // )
         return (
         <div>
-          <div class="col-md-1">
+          <div className="col-md-1">
           </div>
-          <div class="col-md-6" id="home">
+          <div className="col-md-6" id="home">
 
-            <div class="outline">
-              <div class="recipe-share">
-                <div class="panel panel-default">
-                  <div class="panel-body">
-                    <div class="row">
-                      <div class="col-md-10">
+            <div className="outline">
+              <div className="recipe-share">
+                <div className="panel panel-default">
+                  <div className="panel-body">
+                    <div className="row">
+                      <div className="col-md-10">
                         <b>Someone</b>
                           <br/> Yesterday at 3:48pm
                           <br/> <br/> <a href="#">Pumpkin Spice Brownie</a>
                       </div>
 
                     </div>
-                        <div class="row">
-                          <div class="col-md-12">
+                        <div className="row">
+                          <div className="col-md-12">
                             <br/>
                             <a href="#"><img src="img/brownie1.png" width="30%" /></a>
                             <br/> <br/> These brownies are amazing and so easy to make!
                           </div>
                         </div>
                             <hr/>
-                        <div class="row">
-                          <div class="col-md-12">
-                            <ul class="list-inline">
+                        <div className="row">
+                          <div className="col-md-12">
+                            <ul className="list-inline">
                               <li>
                                 Rating:
-                                  <span class="glyphicon glyphicon-star" style="color:red"> </span>
-                                  <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                  <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                  <span class="glyphicon glyphicon-star" style="color:red"></span>
+                                  <span className="glyphicon glyphicon-star" style="color:red"> </span>
+                                  <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                  <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                  <span className="glyphicon glyphicon-star" style="color:red"></span>
                               </li>
                             </ul>
                           </div>
                         </div>
                   </div>
-                          <div class="panel-footer">
-                            <div class="row">
-                              <div class="col-md-12">
-                                <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span> Like</a> · 12 people like this
+                          <div className="panel-footer">
+                            <div className="row">
+                              <div className="col-md-12">
+                                <a href="#"><span className="glyphicon glyphicon-thumbs-up"></span> Like</a> · 12 people like this
                               </div>
                             </div>
                                 <hr/>
-                                <ul class="media-list">
-                                  <li class="media">
-                                    <div class="media-body">
+                                <ul className="media-list">
+                                  <li className="media">
+                                    <div className="media-body">
                                       <b>Someone Else</b> Wow looks good!
-                                        <br/> <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span> Like</a> · 20 hrs </div>
+                                        <br/> <a href="#"><span className="glyphicon glyphicon-thumbs-up"></span> Like</a> · 20 hrs </div>
                                   </li>
-                                  <li class="media">
-                                    <div class="media-body">
+                                  <li className="media">
+                                    <div className="media-body">
                                       <b>Different Someone Else</b> Can't wait to try this recipe
-                                        <br/> <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span> Like</a> · 19 hrs </div>
+                                        <br/> <a href="#"><span className="glyphicon glyphicon-thumbs-up"></span> Like</a> · 19 hrs </div>
                                   </li>
-                                  <li class="media">
-                                    <div class="media-body">
-                                      <div class="input-group">
-                                        <input type="text" class="form-control" placeholder="Write a comment...">
+                                  <li className="media">
+                                    <div className="media-body">
+                                      <div className="input-group">
+                                        <input type="text" className="form-control" placeholder="Write a comment..."/>
                                       </div>
                                     </div>
                                   </li>
@@ -94,61 +94,61 @@ export default class Feed extends React.Component {
         </div>
 
 
-            <div class="col-md-4">
-              <div class="outline">
-                    <div class="panel panel-default pop">
-                        <div class="row">
-                            <div class="col-md-12">
+            <div className="col-md-4">
+              <div className="outline">
+                    <div className="panel panel-default pop">
+                        <div className="row">
+                            <div className="col-md-12">
                                 <h1><center>Most Popular</center></h1><br/>
-                                <ul class="media-list">
-                                    <li class="media">
-                                        <div class="media-left media-top heart">
-                                            <span class="glyphicon glyphicon-heart-empty"></span>
+                                <ul className="media-list">
+                                    <li className="media">
+                                        <div className="media-left media-top heart">
+                                            <span className="glyphicon glyphicon-heart-empty"></span>
                                         </div>
-                                        <div class="media-body">
+                                        <div className="media-body">
                                             <a href="#">Mac and Cheese</a>: Three cheese mac and cheese
                                             <br/>
                                             Avg. Rating:
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
                                         </div>
                                     </li>
-                                    <li class="media">
-                                        <div class="media-left media-top heart">
-                                            <span class="glyphicon glyphicon-heart-empty"></span> </div>
-                                        <div class="media-body">
+                                    <li className="media">
+                                        <div className="media-left media-top heart">
+                                            <span className="glyphicon glyphicon-heart-empty"></span> </div>
+                                        <div className="media-body">
                                             <a href="#">Brownie</a>: Pumpkin spice brownie
                                             <br/>
                                             Avg. Rating:
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
                                         </div>
                                     </li>
-                                    <li class="media">
-                                        <div class="media-left media-top heart">
-                                            <span class="glyphicon glyphicon-heart-empty"></span> </div>
-                                        <div class="media-body">
+                                    <li className="media">
+                                        <div className="media-left media-top heart">
+                                            <span className="glyphicon glyphicon-heart-empty"></span> </div>
+                                        <div className="media-body">
                                             <a href="#">Pasta</a>: Shrimp scampi
                                             <br/>
                                             Avg. Rating:
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
-                                            <span class="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
+                                            <span className="glyphicon glyphicon-star" style="color:red"></span>
                                         </div>
                                     </li>
-                                    <li class="media">
-                                        <div class="media-left media-top">
-                                            <span class="caret"></span>
+                                    <li className="media">
+                                        <div className="media-left media-top">
+                                            <span className="caret"></span>
                                           </div>
-                                        <div class="media-body"> <a href="#">See More</a>
+                                        <div className="media-body"> <a href="#">See More</a>
                                         </div>
                                     </li>
                                 </ul>
@@ -159,8 +159,7 @@ export default class Feed extends React.Component {
               <li role="presentation">DEBUG</li>
               <li role="presentation" id="fb-db-reset"></li>
             </div>
-
-            <div class="col-md-1">
+            <div className="col-md-1">
           </div>
         </div>
         )
