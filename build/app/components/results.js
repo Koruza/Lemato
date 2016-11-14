4cba9906816ebcc9f48613d514e3cfcdd3e3dd77
Albert Xu <thealbertxu@gmail.com>
2016-11-13 19:04:56 -0500

    results chefpoints buttons

--- build/app/components/results.js
+++ build/app/components/results.js
@@ -7,42 +7,66 @@ export default class results extends React.Component {
     this.state = props.data
   }
 
+  handleLikeClick(clickEvent) {
+    clickEvent.preventDefault();
+    if (clickEvent.button === 0) {
+      var callbackFunction = (updatedChefPointsCounter) => {
+          this.setState({chefPoints: updatedChefPointsCounter});
+      };
+      likeRecipe(this.state._id, 1, callbackFunction);
+    }
+  }
+
+  handleDislikeClick(clickEvent) {
+    clickEvent.preventDefault();
+    if (clickEvent.button === 0) {
+      var callbackFunction = (updatedChefPointsCounter) => {
+          this.setState({chefPoints: updatedChefPointsCounter});
+      };
+      unlikeRecipe(this.state._id, 1, callbackFunction);
+    }
+  }
+
   render() {
     var data = this.state
 
     return (
       <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8">
-            <div className="panel panel-success">
-              <div className="panel-body">
-                <div className="media">
-                  <div className="media-left">
-                    <Link to={"/recipes/" + data.name}>
-                      <img className="media-object result-pic" src="img/brownie-egg.jpg" alt="brownie-egg"></img>
-                    </Link>
-                  </div>
-                  <div className="media-body">
-                    <h4 className="media-heading">
-                      <Link to={"/recipes/" + data.name}>{data.name}</Link>
-                    </h4>
-                    {data.description}
-                  </div>
-                  <div className="media-right recipe-vote">
-                    <a href="#"><span className="glyphicon glyphicon-arrow-up"></span></a>
-                    <div>{data.chefPoints}</div>
-                    <a href="#"><span className="glyphicon glyphicon-arrow-down"></span></a>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-2"></div>
+            <div className="col-md-8">
+              <div className="panel panel-success">
+                <div className="panel-body">
+                  <div className="media">
+                    <div className="media-left">
+                      <Link to={"/recipes/" + data.name}>
+                        <img className="media-object result-pic" src="img/brownie-egg.jpg" alt="brownie-egg"></img>
+                      </Link>
+                    </div>
+                    <div className="media-body">
+                      <h4 className="media-heading">
+                        <Link to={"/recipes/" + data.name}>{data.name}</Link>
+                      </h4>
+                      {data.description}
+                    </div>
+                    <div className="media-right recipe-vote">
+                      <a href="#" onClick={(e) => this.handleLikeClick(e)}>
+                        <span className="glyphicon glyphicon-arrow-up"></span>
+                      </a>
+                      <div>{data.chefPoints}</div>
+                      <a href="#" onClick={(e) => this.handleDislikeClick(e)}>
+                        <span className="glyphicon glyphicon-arrow-down"></span>
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        <div className="col-md-2"></div>
+          <div className="col-md-2"></div>
+        </div>
       </div>
     </div>
-  </div>
     )
   }
 }
