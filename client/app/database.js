7487950c6c63c2126d42a167c6880c07d0961b9d
Albert Xu <thealbertxu@gmail.com>
2016-12-10 17:34:20 -0500

    more database fixes

--- client/app/database.js
+++ client/app/database.js
@@ -7,122 +7,96 @@ var startupName = "Lemato";
 // Put your mock objects here, as in Workshop 4
 var initialData = {
   "users": {
-        "1": {
-            "_id": 1,
-            "fullName": "Someone",
-            "bio": "brownie enthusiast",
-            "username":"BrownieKing",
-            "password":"chocolate",
-            "cookbook":[1],
-            "feed": 1
-        },
-        "2": {
-            "_id": 2,
-            "fullName": "Someone Else",
-            "bio": "brownie hater",
-            "username":"BrownieMaster",
-            "password":"fudge",
-            "cookbook":[2],
-            "feed": 2
-        }
+    "1": {
+      "_id": 1,
+      "fullName": "Someone",
+      "bio": "brownie enthusiast",
+      "username":"BrownieKing",
+      "password":"chocolate",
+      "cookbook":[1],
+      "feed": 1
     },
-    "recipes":{
-        "1":{
-          "_id": 1,
-          "author": 2,
-          "name": "Brownie",
-          "postDate": 1453668480000,
-          "chefPoints": [1],
-          "ingredients":[
-            "Flour", "Egg", "Cocoa Powder", "Sugar","Butter"
-           ],
-           "pic":"../../img/brownie-egg.jpg",
-           "instructions":[
-              "1. Combine the butter and sugar",
-              "2. Mix the flour and cocoa powder together in a seperate bowl",
-              "3. Whisk the egg into the flour and cocoa powder mixture",
-              "4. Add the butter and sugar to the mix",
-              "5. Pour it into a 9x9 tray"
-           ],
-           "description": "Homestyle brownies",
-           "allergies": ["dairy"],
-           "meal": "dessert",
-           "dietary":["vegetarian"]
+    "2": {
+      "_id": 2,
+      "fullName": "Someone Else",
+      "bio": "brownie hater",
+      "username":"BrownieMaster",
+      "password":"fudge",
+      "cookbook":[2],
+      "feed": 2
+    }
+  },
+  "recipes":{
+    "1":{
+      "_id": 1,
+      "author": 2,
+      "name": "Brownie",
+      "postDate": 1453668480000,
+      "chefPoints": [1],
+      "ingredients": ["Flour", "Egg", "Cocoa Powder", "Sugar","Butter"],
+      "pic":"../../img/brownie-egg.jpg",
+      "instructions":[
+        "1. Combine the butter and sugar",
+        "2. Mix the flour and cocoa powder together in a seperate bowl",
+        "3. Whisk the egg into the flour and cocoa powder mixture",
+        "4. Add the butter and sugar to the mix",
+        "5. Pour it into a 9x9 tray"
+      ],
+      "description": "Homestyle brownies",
+      "allergies": ["dairy"],
+      "meal": "dessert",
+      "dietary": ["vegetarian"],
+      "comments": [
+        {
+          "author": 1,
+          "contents": "Omg have you tried the brownies I posted?!",
+          "postDate": 1453690800000
         },
-        "2":{
-          "_id": 2,
-          "author": 3,
-          "name": "Brownie in an egg",
-          "chefPoints": [2],
-          "ingredients":[
-            "Flour", "Egg", "Cocoa Powder", "Sugar","Baking Powder"],
-           "pic":"None",
-           "instructions":[
-             "1. Mix dry ingredients together",
-             "2. Mix wet ingredients together",
-             "3. Combine the dry ingredients into the wet ones",
-             "4. Pour the mixture into the empty eggshells"
-           ],
-           "description":"Brownies makes inside an egg shell!",
-           "allergies": ["dairy"],
-           "meal": "dessert",
-           "dietary":["vegetarian"]
-          }
-      },
-      "feedItems":{
-        "1": {
-          "_id": 1,
-          "contents": {
-                  "_id":1,
-                  "author": 1,
-                  "postDate": 1453668480000,
-                  "contents": "Browniesss."
-              },
-          "comments":
-            [
-              {
-                "author": 2,
-                "contents": "Omg have you tried the brownie in an egg?!",
-                "postDate": 1453690800000
-              },
-              {
-                "author": 1,
-                "contents": "Yes, I did. The idea of it blew my mind!!!",
-                "postDate": 1453790800000
-              }
-            ]
-          },
-
-        "2": {
-          "_id": 2,
-          "contents": {
-                  "_id":2,
-                  "author": 2,
-                  "postDate": 1453668780000,
-                  "contents": "Browniesss in eggshellsssssssss."
-              },
-          "comments":
-            [
-              {
-                "author": 1,
-                "contents": "Omg have you tried the brownies I posted?!",
-                "postDate": 1453690800000
-              },
-              {
-                "author": 2,
-                "contents": "Yes, I did. The idea of it blew my tastebuds away!!!",
-                "postDate": 1453790800000
-              }
-            ]
+        {
+          "author": 2,
+          "contents": "Yes, I did. The idea of it blew my tastebuds away!!!",
+          "postDate": 1453790800000
         }
-      },
-      "feeds": {
-        "1": {
-          "_id": 1,
-          "contents": [1]
+      ]
+    },
+    "2": {
+      "_id": 2,
+      "author": 1,
+      "name": "Brownie in an egg",
+      "chefPoints": [2],
+      "ingredients": ["Flour", "Egg", "Cocoa Powder", "Sugar","Baking Powder"],
+      "pic":"None",
+      "instructions": [
+        "1. Mix dry ingredients together",
+        "2. Mix wet ingredients together",
+        "3. Combine the dry ingredients into the wet ones",
+        "4. Pour the mixture into the empty eggshells"
+      ],
+      "description":"Brownies makes inside an egg shell!",
+      "allergies": ["dairy"],
+      "meal": "dessert",
+      "dietary":["vegetarian"],
+      "comments": [
+        {
+          "author": 2,
+          "contents": "Omg have you tried the brownie in an egg?!",
+          "postDate": 1453690800000
+        },
+        {
+          "author": 1,
+          "contents": "Yes, I did. The idea of it blew my mind!!!",
+          "postDate": 1453790800000
         }
-      }
-}
+      ]
+    }
+  },
+  "feeds": {
+    "1": {
+      "_id": 1,
+      "contents": [1]
+    }
+  }
+};
 
 var data = JSON.parse(localStorage.getItem(startupName));
 if (data === null) {
